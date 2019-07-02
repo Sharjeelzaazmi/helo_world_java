ab580969f985679fc518418165349ebda4c3e44c
sharjeelzaazmi <sharjeel@planetbeyond.co.uk>
2019-07-02 11:49:09 +0500

    sum functionality is added

--- src/helo_world_java/Test.java
+++ src/helo_world_java/Test.java
@@ -5,8 +5,12 @@ public class Test
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-		System.out.println("Helo World");
+
+		int a = 1;
+		int b = 1;
+		int c = a + b;
+
+		System.out.println("Helo World , Sum is : " + c);
 
 	}
 
